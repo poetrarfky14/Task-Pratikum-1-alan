cca69bd0756985e1e164b2c7131b8842243cd05b
poetrarfky14 <alanrfky@gmail.com>
2024-09-25 13:12:13 +0700

    first init github

---
+++ Task Pratikum P1/Tugas 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ main() {
+	char* NAMA = "Muhammad Alan Rifky RajabPutra";
+	char* JURUSAN = "Informatika";
+	char* NIM = "2441029";
+	
+	printf ("NAMA \t\t: %s\n", NAMA);
+	printf ("JURUSAN \t: %s\n", JURUSAN);
+	printf ("NIM \t\t: %s\n", NIM);
+	
+	return 0;
+}
---
+++ Task Pratikum P1/Tugas 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main() {
+	int panjang = 10;
+	int lebar = 7;
+	int luas;
+	
+	luas = panjang * lebar;
+	
+	printf ("panjang = %d cm \n", panjang);
+	printf ("lebar = %d cm \n\n", lebar);
+	printf ("luas persegi panjang adalah %d cm", luas);
+}
+
+
+
